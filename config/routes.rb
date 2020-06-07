d09603fc1fe76e9a5f5a4336f82e9ebcd7c8cb20
winifredf <winifred.dykes@hotmail.com>
2020-06-07 19:15:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get "hello_world", to: "static#hello_world"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
